90bb2c4178b06667bed9eb400092e1df012d51d3
Adrian V <avalacos@gmail.com>
2017-09-08 21:27:31 +1000

    Sandbox Setup and Camera

---
+++ Assets/Scripts/objectMove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectMove: MonoBehaviour {
+
+    public float objectSpeed = 0.5f;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        gameObject.transform.position = gameObject.transform.position + new Vector3(0f, 0f, objectSpeed);
+	}
+}
