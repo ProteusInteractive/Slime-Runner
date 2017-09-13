46d6c4d7ceb5da3dd7d4a27dafa1f2f7252bd51c
ChiefTripodBear <avalacos@gmail.com>
2017-09-13 22:33:09 +1000

    Scrapped Sandbox, implemented hex map
    nothing to see here yet. we now have hex tiles. have to continue working.

---
+++ Assets/Scripts/Hex.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The Hex Class defines the grid position, world space position,
+/// size, neighbours, etc. of a hex tile. However, it does not
+/// interact with Unity directly in any way.
+/// </summary>
+public class Hex
+{
+    public Hex(int q, int r)
+    {
+        this.Q = q;
+        this.R = r;
+        this.S = -(q + r);
+    }
+
+    //Q + R + S = 0
+    //S = -(Q + R)
+
+    public readonly int Q; //Column
+    public readonly int R; //Row
+    public readonly int S;
+
+    static readonly float WIDTH_MULTIPLIER = Mathf.Sqrt(3) / 2;
+
+    /// <summary>
+    /// returns world space position of this hex
+    /// </summary>
+    public Vector3 Position()
+    {
+        float radius = 1f;
+        float height = radius * 2;
+        float width = WIDTH_MULTIPLIER * height;
+
+        float horiz = width;
+        float vert = height * 0.75f;
+
+        return new Vector3
+        (
+        horiz * (this.Q + this.R/2f),
+        0,
+        vert * this.R
+        );
+    }
+
+}
---
+++ Assets/Scripts/Hexmap.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hexmap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        GenerateMap();
+
+    }
+
+    public GameObject HexPrefab;
+
+    public void GenerateMap()
+    {
+        for (int column = 0; column < 10; column++)
+        {
+            for (int row = 0; row < 10; row++)
+            {
+                Hex h = new Hex(column, row);
+
+                //Instantiate a Hex
+                Instantiate(
+                    HexPrefab,
+                    h.Position(),
+                    Quaternion.identity,
+                    this.transform);
+            }
+        }
+    }
+
+}
